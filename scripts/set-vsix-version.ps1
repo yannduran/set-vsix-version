bc40698fe51c1ffb9cd8cbc77eb9b4ba9db365c5
Yann Duran <yd@live.com.au>
2022-03-28 20:32:57 +1100

    Remove function definition

--- scripts/set-vsix-version.ps1
+++ scripts/set-vsix-version.ps1
@@ -1,86 +1,91 @@
-﻿function Set-VsixVersion {
-  param(   
-    [string] $versionNumber,
-    [string] $gitRef,
-    [string] $productionRegex,
-    [string] $developmentVersion,
-    [string] $manifestFilePath,
-    [string] $codeFilePath
-  )
+﻿#######################################
+#Script Title: Set VSIX Version
+#Script File Name: set-vsix-version.ps1 
+#Author: Yann Duran
+#Date Created: 2022-03-28
+#######################################
+
+param(   
+  [string] $versionNumber,
+  [string] $gitRef,
+  [string] $productionRegex,
+  [string] $developmentVersion,
+  [string] $manifestFilePath,
+  [string] $codeFilePath
+)
+$valid = $false
+
+try {
+  #region start
+    Show-DatedMessage "Started at"  
+
+    #region inputs
+      Show-InfoMessage "------"
+      Show-InfoMessage "Inputs"
+      Show-InfoMessage "------"
+      Show-InfoMessage " - version-number      = $versionNumber" 
+      Show-InfoMessage " - development-version = $developmentVersion"
+      Show-InfoMessage " - production-version  = $productionVersion"
+      Show-InfoMessage " - production-regex    = $productionRegex"
+      Show-InfoMessage " - git-ref             = $gitRef"
+      Show-InfoMessage " - manifest-file-path  = $manifestFilePath"
+      Show-InfoMessage " - code-file-path      = $codeFilePath"
+    #endregion inputs
+
+    #region constant values      
+      $versionRegex = '([0-9\\.]+)'
+      $manifestRegex = 'Version="' + $versionRegex + '" Language=' # do this all inside ""?
+      $codeRegex = 'Version = "' + $versionRegex + '"' # do this all inside ""?
+      $invalidInputs = "'version-number' was not specified, therefore "
+        + "'github-ref', 'production-regex' and 'development-version' "
+        + "are all required"
+      $missingManifestFile = "A valid 'manifest-file-path' MUST be specified to be able to set the version"
+    #endregion constant values
+
+    #region variable values
+      $versionToSet = '0.1' #default value
+    #endregion variable values
+  #endregion start
+
+  #region process
+    $valid = Test-Inputs $versionNumber, $gitRef, $productionRegex, $developmentVersion
+    if ($valid -eq $false) { Show-ErrorMessage $invalidInputs }
+    
+    $valid = Confirm-FileExists($manifestFilePath)
+    if ($valid -eq $false) { Show-ErrorMessage $missingManifestFile }
+
+    $branch = Get-GitBranch(gitRef)
+    $tag = Get-GitTag(gitRef)
+    $codeFileExists = Confirm-FileExists($codeFilePath)
+    $versionSpecified = ($versionNumber -ne '')
+
+    #temporary
+    $versionToSet = $versionNumber
+  #endregion process
+
+  #region end
+    if ($valid -eq $true) {
+      Write-Output "::set-output name=version-number::$versionToSet"
+
+      Show-VersionResults $manifestVersionBefore $manifestVersionAfter $codeFileExists $codeVersionBefore $codeVersionAfter
+    }
+
+    Show-DatedMessage "Ended at"
+  #endregion end
+}
+catch [System.ArgumentException] {
+  Show-ErrorMessage $_
   $valid = $false
-
-  try {
-    #region start
-      Show-DatedMessage "Started at"  
-
-      #region inputs
-        Show-InfoMessage "------"
-        Show-InfoMessage "Inputs"
-        Show-InfoMessage "------"
-        Show-InfoMessage " - version-number      = $versionNumber" 
-        Show-InfoMessage " - development-version = $developmentVersion"
-        Show-InfoMessage " - production-version  = $productionVersion"
-        Show-InfoMessage " - production-regex    = $productionRegex"
-        Show-InfoMessage " - git-ref             = $gitRef"
-        Show-InfoMessage " - manifest-file-path  = $manifestFilePath"
-        Show-InfoMessage " - code-file-path      = $codeFilePath"
-      #endregion inputs
-
-      #region constant values      
-        $versionRegex = '([0-9\\.]+)'
-        $manifestRegex = 'Version="' + $versionRegex + '" Language=' # do this all inside ""?
-        $codeRegex = 'Version = "' + $versionRegex + '"' # do this all inside ""?
-        $invalidInputs = "'version-number' was not specified, therefore "
-          + "'github-ref', 'production-regex' and 'development-version' "
-          + "are all required"
-        $missingManifestFile = "A valid 'manifest-file-path' MUST be specified to be able to set the version"
-      #endregion constant values
-
-      #region variable values
-        $versionToSet = '0.1' #default value
-      #endregion variable values
-    #endregion start
-  
-    #region process
-      $valid = Test-Inputs $versionNumber, $gitRef, $productionRegex, $developmentVersion
-      if ($valid -eq $false) { Show-ErrorMessage $invalidInputs }
-      
-      $valid = Confirm-FileExists($manifestFilePath)
-      if ($valid -eq $false) { Show-ErrorMessage $missingManifestFile }
-
-      $branch = Get-GitBranch(gitRef)
-      $tag = Get-GitTag(gitRef)
-      $codeFileExists = Confirm-FileExists($codeFilePath)
-      $versionSpecified = ($versionNumber -ne '')
-
-      #temporary
-      $versionToSet = $versionNumber
-    #endregion process
-
-    #region end
-      if ($valid -eq $true) {
-        Write-Output "::set-output name=version-number::$versionToSet"
-
-        Show-VersionResults $manifestVersionBefore $manifestVersionAfter $codeFileExists $codeVersionBefore $codeVersionAfter
-      }
-
-      Show-DatedMessage "Ended at"
-    #endregion end
-  }
-  catch [System.ArgumentException] {
-    Show-ErrorMessage $_
-    $valid = $false
-  }
-  catch [System.InvalidOperationException] {
-    Show-ErrorMessage $_
-    $valid = $false
-  }
-  catch {
-    Show-ExceptionMessage $_
-    Show-ExceptionMessage $_.ScriptStackTrace
-    $valid = $false
-  }
-  finally {
-    if ($valid -eq $false) { exit 1 }
-  }
+}
+catch [System.InvalidOperationException] {
+  Show-ErrorMessage $_
+  $valid = $false
+}
+catch {
+  Show-ExceptionMessage $_
+  Show-ExceptionMessage $_.ScriptStackTrace
+  $valid = $false
+}
+finally {
+  if ($valid -eq $false) { exit 1 }
 }
