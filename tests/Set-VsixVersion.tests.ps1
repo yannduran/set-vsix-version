73094572f3cc589ff97cae3b86a3fdf6d89e997d
Yann Duran <yd@live.com.au>
2022-04-08 23:50:31 +1000

    Add `Set-VsixVersion` tests (for debugging)

---
+++ tests/Set-VsixVersion.tests.ps1
@@ -0,0 +1,27 @@
+BeforeAll { 
+  . ./src/vsix-version-functions.ps1
+  . ./src/Set-VsixVersion.ps1
+}
+
+Describe "Set-VsixVersion" {
+  Context "has version number and manifest file" {
+    It "returns null" {
+      $versionNumber = '1.2.3'
+      $gitRef = ''
+      $productionRegex = ''
+      $versionRegex = ''
+      $developmentVersion = ''
+      $manifestFilePath = './tests/test.vsixmanifest'
+
+      $result = Set-VsixVersion `
+        -versionNumber $versionNumber `
+        -gitRef $gitRef `
+        -productionRegex $productionRegex `
+        -versionRegex $versionRegex `
+        -developmentVersion $developmentVersion `
+        -manifestFilePath $manifestFilePath
+      
+      $result | Should -Be '::set-output name=version-number::0.1'
+    }
+  }
+}
